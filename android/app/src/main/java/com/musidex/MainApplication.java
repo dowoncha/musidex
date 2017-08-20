0aa1f4db4615d6a5b7df310ac061b38616ee58a6
Dowon Cha <dowoncha.cs@gmail.com>
2017-08-20 10:56:21 -0400

    react native youtube dependency

--- android/app/src/main/java/com/musidex/MainApplication.java
+++ android/app/src/main/java/com/musidex/MainApplication.java
@@ -3,16 +3,19 @@ package com.musidex;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
+import com.mapbox.reactnativemapboxgl.ReactNativeMapboxGLPackage;
+
 // Firebase
 import io.invertase.firebase.RNFirebasePackage;
-// import io.invertase.firebase.admob.RNFirebaseAdMobPackage; //Firebase AdMob
-import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage; // Firebase Analytics
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+
 import io.invertase.firebase.auth.RNFirebaseAuthPackage; // Firebase Auth
 import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage; // Firebase Remote Config
 import io.invertase.firebase.crash.RNFirebaseCrashPackage; // Firebase Crash Reporting
@@ -22,7 +25,6 @@ import io.invertase.firebase.perf.RNFirebasePerformancePackage; // Firebase Perf
 import io.invertase.firebase.storage.RNFirebaseStoragePackage; // Firebase Storage
 
 // Mapbox
-import com.mapbox.reactnativemapboxgl.ReactNativeMapboxGLPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -39,19 +41,21 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new VectorIconsPackage(),
-          new RNFirebasePackage(),  // <-- Add this line
-          // Add these packages as appropriate
-          // new RNFirebaseAdMobPackage(),
-          new RNFirebaseAnalyticsPackage(),
-          new RNFirebaseAuthPackage(),
-          new RNFirebaseRemoteConfigPackage(),
-          new RNFirebaseCrashPackage(),
-          new RNFirebaseDatabasePackage(),
-          // new RNFirebaseMessagingPackage(),
-          new RNFirebasePerformancePackage(),
-          new RNFirebaseStoragePackage(),
-          new ReactNativeMapboxGLPackage()
+            new VectorIconsPackage(),
+            new ReactNativeYouTube(),
+            new ReactNativeMapboxGLPackage(),
+            new RNFirebasePackage(),
+
+            // Add these packages as appropriate
+            // new RNFirebaseAdMobPackage(),
+            new RNFirebaseAnalyticsPackage(),
+            new RNFirebaseAuthPackage(),
+            new RNFirebaseRemoteConfigPackage(),
+            new RNFirebaseCrashPackage(),
+            new RNFirebaseDatabasePackage(),
+            // new RNFirebaseMessagingPackage(),
+            new RNFirebasePerformancePackage(),
+            new RNFirebaseStoragePackage()
       );
     }
   };
